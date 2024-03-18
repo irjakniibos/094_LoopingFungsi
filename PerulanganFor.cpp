08eb565f10f5bf2233a74e0f978882129042382f
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-03-18 14:27:42 +0700

    Looping Menggunakan for

---
+++ PerulanganFor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5];
+
+    for (i = 0; i < 5; i++)
+    {   
+
+        cout << i << ":" << "Irza Yaumil Syahrar" << endl;
+    }
+
+    for(i = 0 ; i < 5 ; i ++){
+        cout << "Masukan Nama = " ;
+        cin >> arr[i];
+    }
+
+    for(i = 0 ; i < 5 ; i++){
+        cout << "Nama ke - " << i << " : " << arr[i] << endl;
+    }
+}
